0b37f330b68dcb44f3ea5ad4e56b18499f38758f
ALXPAV4 <alx.pav42106@gmail.com>
2026-05-10 20:00:16 +0200

    Fix product bug

--- client/src/index.js
+++ client/src/index.js
@@ -309,7 +309,7 @@ function Buy(btn) {
   let productPrice;
   let quantity = 1;
 
-  fetch(`http://localhost:3000/product?id=${productId}`)
+  fetch(`${import.meta.env.VITE_API_URL}/product?id=${productId}`)
     .then((res) => res.json())
     .then((data) => {
       productName = data[0].name;
